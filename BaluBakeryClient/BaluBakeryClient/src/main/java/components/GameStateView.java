0c2f0949cb6d09e431eec3d325c6a8a724f619c9
dgadrian05 <dgadrian05@gmail.com>
2024-03-26 13:07:27 +0200

    stylishing the main  button of the clicker

--- BaluBakeryClient/BaluBakeryClient/src/main/java/components/GameStateView.java
+++ BaluBakeryClient/BaluBakeryClient/src/main/java/components/GameStateView.java
@@ -1,29 +1,62 @@
 package components;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.Arrays;
 
 @SuppressWarnings("serial")
 public class GameStateView extends JPanel {
-    JButton bakingButton;
+
     public GameStateView() {
         super(new FlowLayout(FlowLayout.LEFT));
         this.setPreferredSize(new Dimension(400,450));
         this.setVisible(true);
         this.setBackground(new Color(89, 120, 51)); // Calmer and colder cyan color
 
-        this.bakingButton=new JButton("BAKE!");
-        bakingButton.addActionListener(this::bakeAction);
-        bakingButton.setBackground(new Color(129, 146, 57));
-        bakingButton.setForeground(Color.green);
-        bakingButton.setPreferredSize(new Dimension(125, 140));
-        bakingButton.setFont(bakingButton.getFont().deriveFont(16f));
+        JPanel panelBakeButton= new JPanel(new BorderLayout());
+        panelBakeButton.setBackground(new Color(0,0,0,0));
+        panelBakeButton.setPreferredSize(new Dimension(300,300));
+        panelBakeButton.add(createBakingButton(), BorderLayout.SOUTH);
+        add(panelBakeButton);
 
-        add(bakingButton);
+
+        JPanel panelSpacing=new JPanel();
+        panelSpacing.setBackground(new Color(0,0,0,0));
+        panelSpacing.setPreferredSize(new Dimension(400,500));
+        panelSpacing.add(panelBakeButton,BorderLayout.CENTER);
+        add(panelSpacing);
+
+
+
     }
 
+
+    private JButton createBakingButton()
+    {
+        try {
+            Image image = ImageIO.read(new File("src/main/resources/Cakes/Cookie.png"));
+
+            JButton bakingButton=new JButton(new ImageIcon(image));
+            bakingButton.addActionListener(this::bakeAction);
+            bakingButton.setBackground(new Color(0, 0, 0,0));
+
+            return bakingButton;
+
+        } catch(Exception e) {
+            System.out.println("Error: " + Arrays.toString(e.getStackTrace()));
+        }
+
+        return null;
+
+    }
+
+
     private void bakeAction(ActionEvent a) {
+
+        System.out.println("You just baked a cookie!");
 
     }
 }
